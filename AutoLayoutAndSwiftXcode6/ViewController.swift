14d1fcdd2a578eeb1389c4da2023df71d1bc9e51
hsg <hsg@rainsoft.com>
2015-08-15 10:57:52 +0800

    Initial Commit

---
+++ AutoLayoutAndSwiftXcode6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutAndSwiftXcode6
+//
+//  Created by imac on 15/8/15.
+//  Copyright (c) 2015年 com.rainsoft.eyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
