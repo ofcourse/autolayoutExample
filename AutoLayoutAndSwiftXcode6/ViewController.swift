9e8ad0ffc8982b3597ef95549743a66421581e00
hsg <hsg@rainsoft.com>
2015-08-20 17:33:51 +0800

    拖动回弹效果

--- AutoLayoutAndSwiftXcode6/ViewController.swift
+++ AutoLayoutAndSwiftXcode6/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var middleViewTopSpaceLayoutConstant:CGFloat!
+    var middleViewOriginY:CGFloat!
+    
+    @IBOutlet var panGesture: UIPanGestureRecognizer!
+    @IBOutlet weak var middleView: UIView!
+    
+    @IBOutlet weak var middleViewTopSpaceLayout: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        middleViewTopSpaceLayoutConstant=middleViewTopSpaceLayout.constant
+        middleViewOriginY=middleView.frame.origin.y
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pan(sender: AnyObject) {
+        
+        if panGesture.state == UIGestureRecognizerState.Ended{
+            
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
+                self.middleView.frame.origin.y=self.middleViewOriginY
+
+            }, completion: { (success) -> Void in
+                if success{
+                    //些处加不加没什么意义
+                    self.middleViewTopSpaceLayout.constant=self.middleViewTopSpaceLayoutConstant
+                }
+            })
+            
+            
+            return
+        }
+        let y=panGesture.translationInView(self.view).y
+        
+        middleViewTopSpaceLayout.constant=middleViewTopSpaceLayoutConstant + y
+
+        
+    }
 
 }
 
